58d048cfebb7d7212df86e0e4ecccf897358d136
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2020-12-31 16:17:13 -0500

    added full calculation for change, average change, increase, decrease, etc.

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,32 +8,48 @@ budget_csv = os.path.join("Resources", "budget_data.csv")
 print("Financial Analysis")
 print("-------------------------")
 
+# with open(budget_csv) as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter = ",")
+
+#     csv_header = next(csvfile)
+
+#     months = list(csvreader)
+#     count = len(months)
+
+# with open(budget_csv) as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter = ",")
+
+#     csv_header = next(csvfile)
+#     total = 0
+#     for row in csvreader:
+#         total += int(row[1])
+
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
-
+    
+    profit_loss = []
+    change = []
+    date = []
     csv_header = next(csvfile)
-
-    months = list(csvreader)
-    count = len(months)
-
-with open(budget_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
-
-    csv_header = next(csvfile)
-    total = 0
+    
     for row in csvreader:
-        total += int(row[1])
+        date.append(row[0])
+        profit_loss.append(int(row[1]))
 
-with open(budget_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
-    
-    csv_header = next(csvfile)
-    
-    max_increase = max(csvreader, key=lambda row: int(row[1]))
+    num_dates = len(date)
+    total_profit_loss = sum(profit_loss)
 
-average = total / count
+    for row in range(1,len(profit_loss)):
+        change.append(profit_loss[row] - profit_loss[row-1])
 
-print(f"Total Months: {count}")
-print(f"Total: ${total}") 
-print(f"Average Change: ${average}")
-print(f"Greatest Increase: {str(max_increase)}")
+        max_change = max(change)
+        min_change = min(change)
+
+        total_change = sum(change)
+        average_change = total_change / len(change)
+
+print(f"Total Months: {num_dates}")
+print(f"Total: ${total_profit_loss}") 
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase: {max_change}")
+print(f"Greatest Decrease: {min_change}")
