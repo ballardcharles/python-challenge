118fb9e290910d87deda1b9a1cd998ecc9aa32a3
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2021-01-03 10:32:03 -0500

    added script to create txt file with election results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -28,6 +28,7 @@ with open(poll_csv) as csvfile:
 
 max_votes = candidates[candidate_votes.index(max(candidate_votes))]
 
+
 print("Election Results")
 print("------------------------")
 print(f"Total Votes: {vote_total}")
@@ -37,3 +38,18 @@ for row in range(len(candidates)):
 print("------------------------")
 print(f"Winner: {max_votes}")
 print("------------------------")
+
+# Specify the file to write to
+output = os.path.join("Analysis", "PyPoll.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output, 'w') as txtfile:
+    txtfile.write("Election Results\n")
+    txtfile.write("------------------------\n")
+    txtfile.write(f"Total Votes: {vote_total}\n")
+    txtfile.write("------------------------\n")
+    for row in range(len(candidates)):
+        txtfile.write(f"{candidates[row]}: {candidate_votes[row]/vote_total*100:.3f}% ({candidate_votes[row]})\n")
+    txtfile.write("------------------------\n")
+    txtfile.write(f"Winner: {max_votes}\n")
+    txtfile.write("------------------------\n")
