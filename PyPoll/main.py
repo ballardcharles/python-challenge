1cf02a2d4a0e25b740332250b2f30498869da64e
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2021-01-03 08:40:15 -0500

    creating script to count candidates and votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,13 +5,26 @@ import csv
 # create path to read csv file
 poll_csv = os.path.join("Resources", "election_data.csv")
 
+# define variables
+vote_total = 0
+candidates = []
+candidate_votes = []
+
 with open(poll_csv) as csvfile:
+
     poll_data = csv.reader(csvfile, delimiter = ",")
 
-    total_votes = []
     next(csvfile)
 
     for row in poll_data:
-        total_votes.append(row[2])
+        vote_total = vote_total + 1
 
-print(len(total_votes))
+        if row[2] in candidates:
+            candidate_index = candidates.index(row[2])
+            candidate_votes[candidate_index] = candidate_votes[candidate_index] + 1
+        else:
+            candidates.append(row[2])
+            candidate_votes.append(1)
+
+print(vote_total)
+print(candidate_votes)
